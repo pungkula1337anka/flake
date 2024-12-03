ebcaf858e3749c97c5d9fb9c896dc128efc105d9
pungkula <105579081+pungkula1337anka@users.noreply.github.com>
2024-12-03 18:27:25 +0100

    .

--- modules/services/snapcast-client.nix
+++ modules/services/snapcast-client.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs,  ... }:
 {
   systemd.user.services.snapclient-local = {
     wantedBy = [
@@ -8,7 +8,7 @@
       "pipewire.service"
     ];
     serviceConfig = {
-      ExecStart = "${pkgs.snapcast}/bin/snapclient -h 192.168.1.111";
+      ExecStart = "${pkgs.snapcast}/bin/snapclient -h ::1";
     };
   };
 }
